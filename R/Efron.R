08e6cf398c053029906ef9366c07ae68511733c6
gennaro-tedesco <gennarotedesco@gmail.com>
2016-09-11 16:19:15 +0200

    modified the function to allow for no probabilities as input (asymmetric Efron case)

--- R/Efron.R
+++ R/Efron.R
@@ -16,36 +16,65 @@ is.winner <- function(first, second, prob, error){
   comb <- as.data.table(expand.grid(first, second))
   comb$winner <- ifelse(comb$Var1 > comb$Var2, "first", "second")
   results <- comb[, .N, by = winner][, freq:= N/sum(N)]
-  return(results[winner == "first", freq > prob-abs(error) && freq<prob+abs(error)])
+  if(!missing(prob)){
+    return(results[winner == "first", freq > prob-abs(error) && freq<prob+abs(error)])
+  } else {
+    return(length(results[winner == "first", freq]) > 0 && results[winner == "first", freq] > results[winner == "second", freq])
+  }
 }
 
 
 # checks if one set of randomly generated dice is Efron's
 efron.check <- function(dice, faces, prob, error){
     # check for arguments
-    if(dice < 2 || faces < dice || prob < 0 || prob > 1 || dice%%1!=0 || faces%%1!=0){
-      stop("Please check the validity of the arguments that you have assigned. Probabilities must be 0 <= P <= 1, dice < 2 and faces < dice must be integers.")
-    }
+    if(missing(prob)){
+      if(dice < 2 || faces < dice || dice%%1!=0 || faces%%1!=0){
+        stop("Please check the validity of the arguments that you have assigned. Probabilities must be 0 <= P <= 1, dice < 2 and faces < dice must be integers.")
+      }
+      dice_list <- dice_list.generator(dice, faces)
+      #print(dice_list)
 
-    dice_list <- dice_list.generator(dice, faces)
-    #print(dice_list)
-
-    truth <- vector(mode="logical", length=0)
-    for(j in 1:dice){
-      #print(dice_list[,j])
-      #print(dice_list[,j+1])
-      truth <- cbind(truth, is.winner(dice_list[,j], dice_list[,j+1], prob = prob, error = error))
-      #print(truth)
-      #print(all(truth))
-    }
-    if(is.na(all(truth)) || !all(truth)){
-      values <- NULL
+      truth <- vector(mode="logical", length=0)
+      for(j in 1:dice){
+        print(dice_list[,j])
+        print(dice_list[,j+1])
+        truth <- cbind(truth, is.winner(dice_list[,j], dice_list[,j+1], error = error))
+        print(truth)
+        print(all(truth))
+      }
+      if(is.na(all(truth)) || !all(truth)){
+        values <- NULL
+      } else {
+        dice_list <- dice_list[, -(dice+1)]
+        values <- as.data.table(dice_list)
+        colnames(values) <- paste0("die_", 1:dice)
+      }
+      values
     } else {
-      dice_list <- dice_list[, -(dice+1)]
-      values <- as.data.table(dice_list)
-      colnames(values) <- paste0("die_", 1:dice)
+      if(dice < 2 || faces < dice || prob < 0 || prob > 1 || dice%%1!=0 || faces%%1!=0){
+        stop("Please check the validity of the arguments that you have assigned. Probabilities must be 0 <= P <= 1, dice < 2 and faces < dice must be integers.")
+      }
+
+      dice_list <- dice_list.generator(dice, faces)
+      #print(dice_list)
+
+      truth <- vector(mode="logical", length=0)
+      for(j in 1:dice){
+        #print(dice_list[,j])
+        #print(dice_list[,j+1])
+        truth <- cbind(truth, is.winner(dice_list[,j], dice_list[,j+1], prob = prob, error = error))
+        #print(truth)
+        #print(all(truth))
+      }
+      if(is.na(all(truth)) || !all(truth)){
+        values <- NULL
+      } else {
+        dice_list <- dice_list[, -(dice+1)]
+        values <- as.data.table(dice_list)
+        colnames(values) <- paste0("die_", 1:dice)
+      }
+      values
     }
-    values
 }
 
 
@@ -53,12 +82,21 @@ efron.check <- function(dice, faces, prob, error){
 #' @export
 efron.generator <- function(dice, faces, prob, error = 0.001){
   start_time <- proc.time()
-  repeat{
-    z <- efron.check(dice, faces, prob = prob, error = error)
-    delta_time <- proc.time() - start_time
-    if(!is.null(z)) break
+  if(missing(prob)){
+    repeat{
+      z <- efron.check(dice, faces, error = error)
+      delta_time <- proc.time() - start_time
+      if(!is.null(z)) break
+    }
+    return(z)
+  } else {
+    repeat{
+      z <- efron.check(dice, faces, prob = prob, error = error)
+      delta_time <- proc.time() - start_time
+      if(!is.null(z)) break
+    }
+    return(z)
   }
-  return(z)
 }
 
 
